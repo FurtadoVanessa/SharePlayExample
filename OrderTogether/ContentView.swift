2c1fa690039ecc8e20b3e4447b3740aaab0f5970
vanessa.fagundes <vanessa.fagundes@ifood.com.br>
2024-04-30 18:33:36 -0300

    Initial Commit

---
+++ OrderTogether/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OrderTogether
+//
+//  Created by Vanessa Fagundes on 30/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OrderTogether/OrderTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderTogetherApp.swift
+//  OrderTogether
+//
+//  Created by Vanessa Fagundes on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct OrderTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
